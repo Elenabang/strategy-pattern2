6c425dba4a5a60c1a6b15f94f02af01f932d7357
Malena-AV <m.alena07082003@gmail.com>
2022-01-22 14:00:24 +0300

    Фикс библиотеки и консоли

---
+++ Duck/SimUDuck23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Library;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardduck = new MallardDuck();
+            Duck redheadduck = new RedheadDuck();
+
+            Duck[] duck = new Duck[] { mallardduck, redheadduck };
+
+            for (int i = 0; i < duck.Length; i++)
+            {
+                Console.WriteLine(duck[i].Quack());
+                Console.WriteLine(duck[i].Swim());
+                Console.WriteLine(duck[i].Display());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
